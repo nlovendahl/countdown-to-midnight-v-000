45bef63a5e39e47f56f861d1a9ad41a88d780277
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 17:52:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,12 @@
 #write your code here
 
 def countdown(x)
-  until x == 0
+  until x = 0
     puts "#{x} SECOND(S)!"
     x += 1
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  until x = 0
